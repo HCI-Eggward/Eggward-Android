85eb36ab4d68b0259ac26d677a12d32842cbd225
emili5 <anjeongmin05@ewhain.net>
2022-06-04 22:24:48 +0900

    feat: add pet collection

--- app/src/main/java/com/example/eggward/MyPets/MyPetListActivity.java
+++ app/src/main/java/com/example/eggward/MyPets/MyPetListActivity.java
@@ -1,20 +1,32 @@
 package com.example.eggward.MyPets;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import com.example.eggward.Backlogs.BacklogActivity;
 import com.example.eggward.EggBreeding.EggBreedActivity;
 import com.example.eggward.R;
 import com.example.eggward.Schedule.ScheduleActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+
 public class MyPetListActivity extends AppCompatActivity {
+    private GridView gridView;
+    private MyPetListAdapter adapter;
 
     BottomNavigationView navigationView;
 
@@ -45,5 +57,28 @@ public class MyPetListActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        gridView = (GridView) findViewById(R.id.mypetListArea);
+
+        adapter = new MyPetListAdapter();
+        adapter.getMypetList();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                adapter.addItem();
+                gridView.setAdapter(adapter);
+            }
+        },3000);
+
+       gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+           @Override
+           public void onItemClick(AdapterView parent, View v, int position, long id){
+               Intent intent = new Intent(getApplicationContext(),MyPetDetailActivity.class);
+               startActivity(intent);
+           }
+       });
     }
+
+
 }
