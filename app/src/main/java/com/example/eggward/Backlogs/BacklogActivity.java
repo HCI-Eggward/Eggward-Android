ac8fc1f30763af8f5be7129ae1b6e30d27601486
ceppetto <pcy07111@gmail.com>
2022-06-08 02:06:27 +0900

    feat: add backlog stuff

--- app/src/main/java/com/example/eggward/Backlogs/BacklogActivity.java
+++ app/src/main/java/com/example/eggward/Backlogs/BacklogActivity.java
@@ -3,18 +3,49 @@ package com.example.eggward.Backlogs;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
 import com.example.eggward.EggBreeding.EggBreedActivity;
 import com.example.eggward.MyPets.MyPetListActivity;
 import com.example.eggward.R;
 import com.example.eggward.Schedule.ScheduleActivity;
+import com.example.eggward.Schedule.ScheduleListAdapter;
+import com.example.eggward.Schedule.domain.ChildItem;
+import com.example.eggward.Schedule.domain.ParentItem;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class BacklogActivity extends AppCompatActivity {
+    FirebaseFirestore database = FirebaseFirestore.getInstance();
+
+    ArrayList<String> backlogList;
+
+    Integer categoryIndex = 0;
+
+    // TODO : Intent -> 로그인 정보 받아오기
+    String userEmail = "eggward@ewhain.net";
+    ArrayList<String> categoryList;
+
+    AlertDialog.Builder builder;
+
+    ExpandableListView listView;
+    BacklogListAdapter listAdapter;
+    ArrayList<ParentItem> groupList = new ArrayList<>();
+    ArrayList<ArrayList<ChildItem>> childList = new ArrayList<>();
+    ArrayList<ArrayList<ChildItem>> categoriesList = new ArrayList<>();
 
     BottomNavigationView navigationView;
 
@@ -22,6 +53,19 @@ public class BacklogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backlog);
+
+        categoryList = new ArrayList<>();
+
+        getTodoCategory();
+
+        listView = findViewById(R.id.expandableBacklogList);
+
+        listAdapter = new BacklogListAdapter();
+        listAdapter.parentItems = groupList;
+        listAdapter.childItems = childList;
+
+        listView.setAdapter(listAdapter);
+        listView.setGroupIndicator(null);
 
         navigationView = findViewById(R.id.navigationView);
         navigationView.setSelectedItemId(R.id.navigation_backlog);
@@ -47,4 +91,67 @@ public class BacklogActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void setListItems() {
+        groupList.clear();
+        childList.clear();
+
+        childList.addAll(categoriesList);
+
+        for (int i=0; i<categoryList.size(); i++) {
+            Log.v("here", categoryList.get(i));
+            groupList.add(new ParentItem(categoryList.get(i)));
+        }
+
+        listAdapter.notifyDataSetChanged();
+    }
+
+    public void getTodoCategory() {
+        CollectionReference ref = database.collection("User").document(userEmail).collection("backlogList");
+        ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        categoryList.add(document.getId());
+                    }
+
+                    for (int i=0; i<categoryList.size(); i++) {
+                        categoriesList.add(new ArrayList<ChildItem>());
+                    }
+
+                }
+            }
+        });
+
+        getAllBacklogData();
+    }
+
+    public void getAllBacklogData() {
+        CollectionReference ref = database.collection("User").document(userEmail).collection("backlogList");
+        ref.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        ArrayList tmpList = (ArrayList) document.get("backlog");
+                        if (tmpList != null) {
+                            for (int i = 0; i < tmpList.size(); i++) {
+                                ChildItem item = new ChildItem((String) tmpList.get(i));
+                                categoriesList.get(categoryIndex).add(item);
+                            }
+                            categoryIndex++;
+                            setListItems();
+                        }
+
+                    }
+                } else {
+                    //
+                }
+            }
+        });
+    }
+
+
+
 }
